d8b6533cadc251f4b3f95ae2102f307e8de7c1a0
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2024-01-10 19:19:07 -0500

    Update Day10-2015.py

--- Day10-2015.py
+++ Day10-2015.py
@@ -1,14 +1,14 @@
-def do_thing(input):
+def create_new_sequence(number_string):
     i = 0
     new_string = ""
-    while i < len(input):
-        current_char = input[i]
+    while i < len(number_string):
+        current_char = number_string[i]
         j = i
         streak = 0
-        while j < len(input):
-            if input[j] == current_char:
+        while j < len(number_string):
+            if number_string[j] == current_char:
                 streak += 1
-                if j == len(input) - 1:
+                if j == len(number_string) - 1:
                     new_string = new_string + str(streak) + current_char
             else:
                 new_string = new_string + str(streak) + current_char
@@ -19,9 +19,8 @@ def do_thing(input):
         i += 1
     return new_string
 
-input = "1113222113"
-for i in range(50):
-    print("Processing thing", i)
-    input = do_thing(input)
 
-print(len(input))
+puzzle_input = "1113222113"
+for i in range(50):
+    puzzle_input = create_new_sequence(puzzle_input)
+    print(i, len(puzzle_input))
